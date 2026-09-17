1fee6d128ca2f608ad82a0e4d01bae2b43a5d656
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-09-17 09:12:48 +0600

    feat(utils): add asset paths and toast messaging helper

---
+++ lib/Utils/AppIcons/app_icons.dart
@@ -0,0 +1,51 @@
+class AppIcons {
+  static const String basePath = "assets/icons";
+
+  //static const String logo = "$basePath/logo.svg";
+
+  static const String addressIcon = "$basePath/address_icon.svg";
+  static const String analytic = "$basePath/analytic.svg";
+  static const String analyticActive = "$basePath/analytic_active.svg";
+  static const String calenderIcon = "$basePath/calender_icon.svg";
+  static const String cameraIcon = "$basePath/camera_icon.svg";
+  static const String changePasswordIcon = "$basePath/change_pass_icon.svg";
+  static const String customBackArrow = "$basePath/custom_back_arrow.svg";
+  static const String customDownArrow = "$basePath/custom_down_arrow.svg";
+  static const String dateIcon = "$basePath/date_icon.svg";
+  static const String deleteIcon = "$basePath/delete_icon.svg";
+  static const String editIcon = "$basePath/edit_icon.svg";
+  static const String eventIcon = "$basePath/event.svg";
+  static const String eventActive = "$basePath/event_active.svg";
+  static const String eventCreate = "$basePath/event_creat.svg";
+  static const String eventCreateActive = "$basePath/event_creta_active.svg";
+  static const String eventHistory = "$basePath/event_history.svg";
+  static const String eventHistoryIcon = "$basePath/event_history_iocn.svg";
+  static const String eventTime = "$basePath/event_time.svg";
+  static const String faqIcon = "$basePath/faq_icon.svg";
+  static const String filterColor = "$basePath/filter.svg";
+  static const String filter = "$basePath/filter_normal.svg";
+  static const String following = "$basePath/following.svg";
+  static const String followingActive = "$basePath/following_active.svg";
+  static const String home = "$basePath/home.svg";
+  static const String homeActive = "$basePath/home_active.svg";
+  static const String logOut = "$basePath/l.svg";
+  static const String menu = "$basePath/menu-alt-2.svg";
+  static const String myEventICon = "$basePath/my_event_icon.svg";
+  static const String notification = "$basePath/notification.svg";
+  static const String profile = "$basePath/profile.svg";
+  static const String profileActive = "$basePath/profile_active.svg";
+  static const String profileMenu = "$basePath/profile_menu.svg";
+  static const String rightArrow = "$basePath/right_arrow.svg";
+  static const String saveEvent = "$basePath/save_event.svg";
+  static const String saveEventActive = "$basePath/saveds_event_active.svg";
+  static const String search = "$basePath/search.svg";
+  static const String searchActive = "$basePath/search_active.svg";
+  static const String termsCondition = "$basePath/term_condition.svg";
+  static const String trading = "$basePath/trading.svg";
+  static const String transaction = "$basePath/transaction_icon.svg";
+  static const String vlepo = "$basePath/vlepo.svg";
+  static const String vlepoActive = "$basePath/vlepo_active.svg";
+  static const String tickets = "$basePath/t.svg";
+  static const String earn = "$basePath/earn.svg";
+
+}
---
+++ lib/Utils/AppImg/app_img.dart
@@ -0,0 +1,8 @@
+class AppImages {
+  static const String basePath = "assets/images";
+
+  ///====================== OnBoarding Image =========================
+  static const String vlepoWhite = "$basePath/vlepo_white_logo.png";
+
+
+}
