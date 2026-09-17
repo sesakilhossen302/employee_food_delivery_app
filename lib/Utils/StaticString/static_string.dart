a516ddc7dcba114ec178173f0705d67fb709b81d
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-09-17 09:12:48 +0600

    feat(utils): add color palette, app constants, and static strings

---
+++ lib/Utils/StaticString/static_string.dart
@@ -0,0 +1,18 @@
+class AppString {
+  ///<====================== These are fro the validation ======================>
+
+  static RegExp passRegexp = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.{8,}$)');
+
+  static RegExp urlRegex = RegExp( r'^(http|https):\/\/[a-zA-Z0-9-\.]+\.[a-z]{2,4}(/\S*)?$');
+
+  static RegExp emailRegexp = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+  static const String fieldCanBeEmpty = "Field cannot be empty'";
+
+
+  ///<================= This is for the create account screen ================>
+
+  static const String  createAccount="Create Account";
+
+
+}
